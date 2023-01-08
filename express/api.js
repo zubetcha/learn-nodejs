cff0582b4e1605739751781b48388880f22f2757
zubetcha <zuhye5@gmail.com>
2023-01-08 20:50:57 +0900

    TDD - mocha

---
+++ express/api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const users = [
+  {
+    id: 1,
+    name: 'zubetcha',
+  },
+  {
+    id: 2,
+    name: 'cookie',
+  },
+  {
+    id: 3,
+    name: 'cake',
+  },
+];
+
+app.use(morgan('dev'));
+
+app.get('/users', function (req, res) {
+  res.json(users);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000');
+});
