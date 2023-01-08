e8e1959458a32f22b92880f5acf745590dae8319
zubetcha <zuhye5@gmail.com>
2023-01-08 21:11:21 +0900

    TDD - should

--- express/utils.spec.js
+++ express/utils.spec.js
@@ -1,10 +1,12 @@
 const utils = require('./utils');
 const assert = require('assert');
+const should = require('should');
 
 describe('utils.js 모듈의 capitalize 함수는 ', () => {
   it('문자열의 첫 번째 문자를 대문자로 변환한다.', () => {
     const result = utils.capitalize('hello');
 
-    assert.equal(result, 'Hello');
+    // assert.equal(result, 'Hello');
+    result.should.be.equal('Hello');
   });
 });
