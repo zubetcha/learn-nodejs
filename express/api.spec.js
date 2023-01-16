49f2cb1c5654e055935f1421567e367e851b25f9
zubetcha <zuhye5@gmail.com>
2023-01-16 21:39:45 +0900

    테스트 코드 작성 연습

--- express/api.spec.js
+++ express/api.spec.js
@@ -1,13 +1,32 @@
-const app = require('./api');
 const request = require('supertest');
+const should = require('should');
+
+const app = require('./api');
 
 describe('GET /users는 ', () => {
-  it('...', (done) => {
-    request(app)
-      .get('/users')
-      .end((req, res) => {
-        console.log(res.body);
-        done();
-      });
+  describe('성공 시 ', () => {
+    it('유저 객체를 담은 배열로 응답한다.', (done) => {
+      request(app)
+        .get('/users')
+        .end((req, res) => {
+          res.body.should.be.instanceOf(Array);
+          done();
+        });
+    });
+
+    it('최대 limit 개수만큼 응답한다.', (done) => {
+      request(app)
+        .get('/users?limit=2')
+        .end((req, res) => {
+          res.body.should.have.lengthOf(2);
+          done();
+        });
+    });
+  });
+
+  describe('실패 시 ', () => {
+    it('limit이 숫자형이 아니면 status 400을 응답한다.', (done) => {
+      request(app).get('/users?limit=two').expect(400).end(done);
+    });
   });
 });
