b8ec2a4e0890c8e6d62360847a446304db30f54b
zubetcha <zuhye5@gmail.com>
2023-01-17 23:27:08 +0900

    delete 메서드 테스트코드

--- express/api.js
+++ express/api.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 
 const app = express();
 
-const users = [
+let users = [
   {
     id: 1,
     name: 'zubetcha',
@@ -30,6 +30,34 @@ app.get('/users', function (req, res) {
   }
 
   res.json(users.slice(0, limit));
+});
+
+app.get('/users/:id', function (req, res) {
+  const id = parseInt(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return res.status(404).end();
+  }
+
+  res.json(user);
+});
+
+app.delete('/users/:id', function (req, res) {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).end();
+  }
+
+  users = users.filter((user) => user.id !== id);
+
+  return res.status(204).end();
 });
 
 app.listen(3000, function () {
--- express/api.spec.js
+++ express/api.spec.js
@@ -30,3 +30,39 @@ describe('GET /users는 ', () => {
     });
   });
 });
+
+describe('GET /users/1 은 ', () => {
+  describe('성공 시 ', () => {
+    it('id가 1인 유저 객체를 반환한다.', (done) => {
+      request(app)
+        .get('/users/1')
+        .end((err, res) => {
+          res.body.should.have.property('id', 1);
+          done();
+        });
+    });
+  });
+
+  describe('실패 시 ', () => {
+    it('id가 숫자가 아닐 경우 400으로 응답한다.', (done) => {
+      request(app).get('/users/one').expect(400).end(done);
+    });
+    it('id로 유저를 찾을 수 없을 경우 404로 응답한다.', (done) => {
+      request(app).get('/users/999').expect(404).end(done);
+    });
+  });
+});
+
+describe('DELETE /users/1 은', () => {
+  describe('성공 시 ', () => {
+    it('204를 응답한다.', (done) => {
+      request(app).delete('/users/1').expect(204).end(done);
+    });
+  });
+
+  describe('실패 시 ', () => {
+    it('id가 숫자가 아닐 경우 400으로 응답한다', (done) => {
+      request(app).delete('/users/one').expect(400).end(done);
+    });
+  });
+});
